15f0f6bcc07c0651bac7bb30114fd050c013d542
MrBoatCraft <stephenwest@unr.edu>
2025-04-04 20:35:11 -0700

    Foods

--- Assets/Scripts/Food/FoodSpawner.cs
+++ Assets/Scripts/Food/FoodSpawner.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class FoodSpawner : MonoBehaviour
 {
     public GameObject foodPrefab;
-    public FoodItem[] availableFoods;
+    public GameObject availableFood;
 
     void Start()
     {
