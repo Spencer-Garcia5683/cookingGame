b71ba17335a5a70e704682b48f5ec4608e9be925
MrBoatCraft <stephenwest@unr.edu>
2025-04-04 20:53:19 -0700

    Updated Food items

--- Assets/Scripts/Food/FoodSpawner.cs
+++ Assets/Scripts/Food/FoodSpawner.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class FoodSpawner : MonoBehaviour
 {
     public GameObject foodPrefab;
-    public GameObject availableFood;
+    public FoodItem[] availableFood;
 
     void Start()
     {
