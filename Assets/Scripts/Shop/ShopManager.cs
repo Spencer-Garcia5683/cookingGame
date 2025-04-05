ec2e85a1c82d834ad311a4d870f6f72639ea96c4
MrBoatCraft <stephenwest@unr.edu>
2025-04-05 00:02:52 -0700

    ShopUI

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ShopManager : MonoBehaviour
 {
-    [SerializeField] private StoreManager storeManager;
-    [SerializeField] private ShopItem[] shopItems;
+    [SerializeField] private StoreManager storeManager;  // Reference to StoreManager
+    [SerializeField] private ShopItem[] shopItems;      // Array of shop items
+
+    // This method returns the list of shop items to be used by other classes (e.g., ShopButton)
+    public ShopItem[] GetShopItems()
+    {
+        return shopItems;
+    }
+
+    void Start()
+    {
+
+    }
 
     public void BuyItem(ShopItem item)
     {
-        
         if (storeManager.store.Money >= item.price)
         {
             storeManager.store.SpendMoney(item.price);
+            Debug.Log($"Bought {item.itemName} for ${item.price}. Remaining money: {storeManager.store.Money}");
         }
         else
         {
